adc347685c4901213f3ee1ba696d4e30eaf4fa44
Temo Qutashvili <qutashvili2000@gmail.com>
2023-07-12 14:56:56 +0400

    done

--- BookingService.Application/Booking/Command/CreateBooking/ConsultationModel.cs
+++ BookingService.Application/Booking/Command/CreateBooking/ConsultationModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string? Patient { get; set; }
         public string? Doctor { get; set; }
+        public string? UserName { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Status { get; set; }
